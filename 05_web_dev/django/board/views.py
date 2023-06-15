44bd804acd01d2799182f63d689cb8ab9419ca11
seohyun <shjang827@gmail.com>
2023-06-15 12:47:26 +0900

    web

--- 05_web_dev/django/board/views.py
+++ 05_web_dev/django/board/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question
-from django.http import HttpResponseNotAllowed
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator 
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 # from django.http import HttpResponse
+# from django.http import HttpResponseNotAllowed
 
 def index(request):
     # return HttpResponse("안녕하세요. board의 index 페이지입니다.")
@@ -20,33 +22,52 @@ def detail(request, question_id):
     context = {'question':question}
     return render(request, 'board/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    # question.answer_set.create(content=request.POST.get('content'), created_at=timezone.now())
-    # answer = Answer(question=question, content=request.POST.get('content'), created_at=timezone.now())
-    # answer.save()
     if request.method == 'POST':
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user  # author 속성에 로그인 계정 저장 
             answer.created_at = timezone.now()
             answer.question = question
             answer.save()
             return redirect('board:detail', question_id=question.id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible.')
+        form = AnswerForm()
     context = {'question':question, 'form':form}
     return render(request, 'board/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user  # author 속성에 로그인 계정 저장 
             question.created_at = timezone.now()
             question.save()
             return redirect('board:index')
     else:
         form = QuestionForm()
     context = {'form':form}
+    return render(request, 'board/question_form.html', context)
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '수정 권한이 없습니다.')
+        return redirect('board:detail', question_id=question.id)
+    if request.method == 'POST':
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.modified_at = timezone.now()  # 수정 일시 저장
+            question.save()
+            return redirect('board:detail', question_id=question.id)
+    else:
+        form = QuestionForm(instance=question)
+    context = {'form': form}
     return render(request, 'board/question_form.html', context)
--- 05_web_dev/django/config/urls.py
+++ 05_web_dev/django/config/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-app_name = 'board'
+from board import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('board/', include('board.urls')),
+    path('common/', include('common.urls')),
+    path('', views.index, name='index'),  # '/'에 해당되는 path 
 ]
